50c31ad08c0a3c1ebb59258897a79593bf9b4644
aryamanpunjabi <aryamanpunjabi3670@gmail.com>
2023-02-25 01:03:16 +0530

    secondcommit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const app = express()
-
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+//importing node framework
+var express = require('express');
+ 
+var app = express();//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('hello world');
+});//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+export default app
